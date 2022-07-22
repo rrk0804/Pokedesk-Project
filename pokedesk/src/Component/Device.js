a6178c2f25406dd84c04860048215d7ac9bb5c37
Rajit Khatri <rajitkhtr@gmail.com>
2022-07-22 11:55:13 -0400

    Added Background

---
+++ pokedesk/src/Component/Device.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../Style/DeviceStyle.css'
+
+
+class Device extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    render () {
+        // Type your code here...
+        const message = "Hello";
+
+        // Return some JSX here...
+        return (
+          <div id="card">
+          </div>
+        );
+    }
+}
+
+
+export default Device;
